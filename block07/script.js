6b267a9c167ce944a150cb915feb4fef67d67f86
alexv <phamvillatoro12@gmail.com>
2023-09-07 20:04:02 -0500

    update

--- block07/script.js
+++ block07/script.js
@@ -1,3 +1,4 @@
+const introMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
 
 const firstCode = 20 + (-10);
 
@@ -5,7 +6,7 @@ const secondCode = 80 / 2;
 
 const thirdCode = 39 + 0;
 
-const fullMessage = `${firstCode} - ${secondCode} - ${thirdCode}`;
+const fullMessage = `${introMessage} \n${firstCode} - ${secondCode} - ${thirdCode}`;
 
 document.getElementById('showCode').addEventListener('click', function () {
     alert(fullMessage);
